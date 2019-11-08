0f01428c4c53db83d872a586fd1f5ec45e99d2de
sloonoopootam <saykoea@gmail.com>
2019-11-08 15:40:22 +0200

    Work 4

--- task_04-2.js
+++ task_04-2.js
@@ -1,25 +1,31 @@
 let spiralMatrix = (R, C, r0, c0) => {
-    const result = [];
-    let sign = 1;
-     do {
-         for (let i = 0; i < R; i++) {
-             if (r0 < R && r0 >= 0 && c0 < C && c0 >= 0) {
-                 result.push([r0, c0]);
-             }
-             r0 += sign;
-         }
 
-         for (let j = 0; j < C; j++) {
-             if (r0 < R && r0 >= 0 && c0 < C && c0 >= 0) {
-                 result.push([r0, c0]);
-             }
-             c0 += sign;
-         }
+    const dr = [0, 1, 0, -1];
+    const dc = [1, 0, -1, 0];
 
-         sign *= -1;
-    } while (result.length < (R * C));
+    const coords = {x: r0, y: c0};
+    const result = [[r0, c0]];
 
-    return console.log(result);
+    if (R * C === 1) {
+        return result
+    } else {
+        for (let k = 1; k < 2 * (R + C) && result.length <= (R * C); k += 2) {
+            for (let i = 0; i < 4; i++) {
+                let dk = k + i / 2;
+                for (let j = 0; j < dk; j++) {
+                    coords.x += dr[i];
+                    coords.y += dc[i];
+                    if (coords.x >= 0 && coords.x < R && coords.y >= 0 && coords.y < C) {
+                        result.push([coords.x, coords.y]);
+                    }
+                }
+            }
+        }
+    }
+    return result;
 };
 
-let res1 = spiralMatrix(1, 4, 0, 0);
+// let res1 = spiralMatrix(1, 4, 0, 0);
+// console.log(res1);
+let res2 = spiralMatrix(5, 6, 1, 4);
+    console.log(res2);
